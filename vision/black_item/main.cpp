e136a7d0485e574bde9c81b3a868dcdbc6af8d52
eithwa <mich841012@gmail.com>
2019-07-31 14:04:05 +0800

    fix vision effect

--- vision/black_item/main.cpp
+++ vision/black_item/main.cpp
@@ -12,7 +12,11 @@ int main(int argc, char **argv)
     ros::NodeHandle h_node;
     signal(SIGINT, SigintHandler);
     Vision cam("camera/image_raw");
-    ros::spin();
+    ros::Rate loop_rate(200);
+    while(ros::ok()){
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
     ROS_INFO("Node exit");
     printf("Process exit\n");
     return 0;
